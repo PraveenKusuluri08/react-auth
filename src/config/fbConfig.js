ea2c422784406a7dff7eedda628f64c35f22911c
Praveen kusuluri <76720876+PraveenKusuluri08@users.noreply.github.com>
2022-01-16 16:31:15 +0530

    Update fbConfig.js

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -4,12 +4,7 @@ import "firebase/firestore"
 import "firebase/auth"
 
 var config = {
-  apiKey: "AIzaSyDNJycEHY5z64lbmrygXtENFV3L7NpEzNo",
-  authDomain: "auth-app-e4978.firebaseapp.com",
-  projectId: "auth-app-e4978",
-  storageBucket: "auth-app-e4978.appspot.com",
-  messagingSenderId: "682336724411",
-  appId: "1:682336724411:web:dc17136e91a0da6948509e"
+ 
 };
 
 firebase.initializeApp(config);
